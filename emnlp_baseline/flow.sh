5bbc716564e2cc17d764e4269a5caf7f51fd8874
Yibing Liu <zapplea@gmail.com>
2018-06-02 06:04:16 +1000

    add

--- emnlp_baseline/flow.sh
+++ emnlp_baseline/flow.sh
@@ -4,8 +4,11 @@ if [ $USER = "liu121" ]
 then
     if [ $stage1 = "True" ]
     then
-        # echo rm is forbidden
-        rm -r /datastore/liu121/nosqldb2/emnlp_baseline/model/*
+        echo rm is forbidden
+        rm -r /datastore/liu121/nosqldb2/emnlp_baseline/model_bbn/*
+        rm -r /datastore/liu121/nosqldb2/emnlp_baseline/model_bbn200/*
+        rm -r /datastore/liu121/nosqldb2/emnlp_baseline/model_conll/*
+        rm -r /datastore/liu121/nosqldb2/emnlp_baseline/model_conll200/*
     elif [ $stage1 = "False1" ]
     then
         rm /datastore/liu121/nosqldb2/emnlp_baseline/conlleval_bbn_bbn_kn/*
@@ -43,42 +46,42 @@ then
 #        sbatch run_learn.sh 5 $stage1 "conll"
     elif [ $stage1 = "False0" ]
     then
-        bash run_.sh 0 $stage1 "bbn_bbn_kn"
+        sbatch run_learn.sh 0 $stage1 "bbn_bbn_kn"
 #        sbatch run_learn.sh 1 $stage1 "bbn_bbn_kn"
-        #sbatch run_learn.sh 2 $stage1 "bbn_bbn_kn"
-#        sbatch run_learn.sh 3 $stage1 "bbn_bbn_kn"
+        sbatch run_learn.sh 2 $stage1 "bbn_bbn_kn"
+        sbatch run_learn.sh 3 $stage1 "bbn_bbn_kn"
 #        sbatch run_learn.sh 4 $stage1 "bbn_bbn_kn"
     elif [ $stage1 = "False1" ]
     then
         sbatch run_learn.sh 0 $stage1 "bbn_bbn_kn"
 #        sbatch run_learn.sh 1 $stage1 "bbn_bbn_kn"
         sbatch run_learn.sh 2 $stage1 "bbn_bbn_kn"
-#        sbatch run_learn.sh 3 $stage1 "bbn_bbn_kn"
+        sbatch run_learn.sh 3 $stage1 "bbn_bbn_kn"
 #        sbatch run_learn.sh 4 $stage1 "bbn_bbn_kn"
 
         sbatch run_learn.sh 0 $stage1 "bbn_cadec"
 #        sbatch run_learn.sh 1 $stage1 "bbn_cadec"
         sbatch run_learn.sh 2 $stage1 "bbn_cadec"
-#        sbatch run_learn.sh 3 $stage1 "bbn_cadec"
+        sbatch run_learn.sh 3 $stage1 "bbn_cadec"
 #        sbatch run_learn.sh 4 $stage1 "bbn_cadec"
 #
         sbatch run_learn.sh 0 $stage1 "bbn_nvd"
 #        sbatch run_learn.sh 1 $stage1 "bbn_nvd"
         sbatch run_learn.sh 2 $stage1 "bbn_nvd"
-#        sbatch run_learn.sh 3 $stage1 "bbn_nvd"
+        sbatch run_learn.sh 3 $stage1 "bbn_nvd"
 #        sbatch run_learn.sh 4 $stage1 "bbn_nvd"
 
         sbatch run_learn.sh 0 $stage1 "bbn_cadec_simple"
 #        sbatch run_learn.sh 1 $stage1 "bbn_cadec_simple"
         sbatch run_learn.sh 2 $stage1 "bbn_cadec_simple"
-#        sbatch run_learn.sh 3 $stage1 "bbn_cadec_simple"
+        sbatch run_learn.sh 3 $stage1 "bbn_cadec_simple"
 #        sbatch run_learn.sh 4 $stage1 "bbn_cadec_simple"
     elif [ $stage1 = "False2" ]
     then
         sbatch run_learn.sh 0 $stage1 "conll_bbn_kn"
 #        sbatch run_learn.sh 1 $stage1 "conll_bbn_kn"
         sbatch run_learn.sh 2 $stage1 "conll_bbn_kn"
-#        sbatch run_learn.sh 3 $stage1 "conll_bbn_kn"
+        sbatch run_learn.sh 3 $stage1 "conll_bbn_kn"
 #        sbatch run_learn.sh 4 $stage1 "conll_bbn_kn"
 
 
@@ -89,16 +92,16 @@ then
 #        sbatch run_learn.sh 3 $stage1 "conll_cadec_simple"
 #        sbatch run_learn.sh 4 $stage1 "conll_cadec_simple"
 
-#        sbatch run_learn.sh 0 $stage1 "conll_cadec"
+        sbatch run_learn.sh 0 $stage1 "conll_cadec"
 #        sbatch run_learn.sh 1 $stage1 "conll_cadec"
-#        sbatch run_learn.sh 2 $stage1 "conll_cadec"
-#        sbatch run_learn.sh 3 $stage1 "conll_cadec"
+        sbatch run_learn.sh 2 $stage1 "conll_cadec"
+        sbatch run_learn.sh 3 $stage1 "conll_cadec"
 #        sbatch run_learn.sh 4 $stage1 "conll_cadec"
 #
-#        sbatch run_learn.sh 0 $stage1 "conll_nvd"
+        sbatch run_learn.sh 0 $stage1 "conll_nvd"
 #        sbatch run_learn.sh 1 $stage1 "conll_nvd"
-#        sbatch run_learn.sh 2 $stage1 "conll_nvd"
-#        sbatch run_learn.sh 3 $stage1 "conll_nvd"
+        sbatch run_learn.sh 2 $stage1 "conll_nvd"
+        sbatch run_learn.sh 3 $stage1 "conll_nvd"
 #        sbatch run_learn.sh 4 $stage1 "conll_nvd"
     fi
     #sbatch run_learn.sh 6
