1021f9b53e42671c475009c577fd93c8dad1de66
Yibing Liu <zapplea@gmail.com>
2018-06-04 10:36:31 +1000

    add

--- emnlp_baseline/flow.sh
+++ emnlp_baseline/flow.sh
@@ -62,28 +62,28 @@ then
     elif [ $stage1 = "False1" ]
     then
         sbatch run_learn.sh 0 $stage1 "bbn_bbn_kn"
-#        sbatch run_learn.sh 1 $stage1 "bbn_bbn_kn"
-        sbatch run_learn.sh 2 $stage1 "bbn_bbn_kn"
-        sbatch run_learn.sh 3 $stage1 "bbn_bbn_kn"
-#        sbatch run_learn.sh 4 $stage1 "bbn_bbn_kn"
-
-        sbatch run_learn.sh 0 $stage1 "bbn_cadec"
-#        sbatch run_learn.sh 1 $stage1 "bbn_cadec"
-        sbatch run_learn.sh 2 $stage1 "bbn_cadec"
-        sbatch run_learn.sh 3 $stage1 "bbn_cadec"
-#        sbatch run_learn.sh 4 $stage1 "bbn_cadec"
+##        sbatch run_learn.sh 1 $stage1 "bbn_bbn_kn"
+#        sbatch run_learn.sh 2 $stage1 "bbn_bbn_kn"
+#        sbatch run_learn.sh 3 $stage1 "bbn_bbn_kn"
+##        sbatch run_learn.sh 4 $stage1 "bbn_bbn_kn"
 #
-        sbatch run_learn.sh 0 $stage1 "bbn_nvd"
-#        sbatch run_learn.sh 1 $stage1 "bbn_nvd"
-        sbatch run_learn.sh 2 $stage1 "bbn_nvd"
-        sbatch run_learn.sh 3 $stage1 "bbn_nvd"
-#        sbatch run_learn.sh 4 $stage1 "bbn_nvd"
-
-        sbatch run_learn.sh 0 $stage1 "bbn_cadec_simple"
-#        sbatch run_learn.sh 1 $stage1 "bbn_cadec_simple"
-        sbatch run_learn.sh 2 $stage1 "bbn_cadec_simple"
-        sbatch run_learn.sh 3 $stage1 "bbn_cadec_simple"
-#        sbatch run_learn.sh 4 $stage1 "bbn_cadec_simple"
+#        sbatch run_learn.sh 0 $stage1 "bbn_cadec"
+##        sbatch run_learn.sh 1 $stage1 "bbn_cadec"
+#        sbatch run_learn.sh 2 $stage1 "bbn_cadec"
+#        sbatch run_learn.sh 3 $stage1 "bbn_cadec"
+##        sbatch run_learn.sh 4 $stage1 "bbn_cadec"
+##
+#        sbatch run_learn.sh 0 $stage1 "bbn_nvd"
+##        sbatch run_learn.sh 1 $stage1 "bbn_nvd"
+#        sbatch run_learn.sh 2 $stage1 "bbn_nvd"
+#        sbatch run_learn.sh 3 $stage1 "bbn_nvd"
+##        sbatch run_learn.sh 4 $stage1 "bbn_nvd"
+#
+#        sbatch run_learn.sh 0 $stage1 "bbn_cadec_simple"
+##        sbatch run_learn.sh 1 $stage1 "bbn_cadec_simple"
+#        sbatch run_learn.sh 2 $stage1 "bbn_cadec_simple"
+#        sbatch run_learn.sh 3 $stage1 "bbn_cadec_simple"
+##        sbatch run_learn.sh 4 $stage1 "bbn_cadec_simple"
     elif [ $stage1 = "False2" ]
     then
         sbatch run_learn.sh 0 $stage1 "conll_bbn_kn"
