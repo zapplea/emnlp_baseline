3a026c25a2b6700b3c65a382a3c3f57177552704
Yibing Liu <zapplea@gmail.com>
2018-06-08 20:01:41 +1000

    add

--- multiclass/classifier.py
+++ multiclass/classifier.py
@@ -206,11 +206,9 @@ class Classifier:
             mask = self.lookup_mask(X_id, graph)
             # X.shape = (batch size, words num, feature dim)
             X = self.lookup_table(X_id, mask, graph)
-            print('X.shape: ',X.get_shape())
             with tf.variable_scope('bilstm') as vs:
                 # X.shape = (batch size, max time step, 2*lstm cell size)
                 X = self.bilstm(X, seq_len, graph)
-                print('H.shape: ',X.get_shape())
                 graph.add_to_collection('bilstm_reg',
                                         tf.contrib.layers.l2_regularizer(self.nn_config['reg_rate'])(
                                             graph.get_tensor_by_name(
@@ -221,10 +219,8 @@ class Classifier:
                                                 'bilstm/bidirectional_rnn/bw/basic_lstm_cell/kernel:0')))
 
             soft_log_mask = tf.reshape(self.softmax_log_mask(X_id, graph),shape=(-1,))
-            print('soft_log_mask.shape: ', soft_log_mask.get_shape())
             Y_one_hot = self.Y_2one_hot(Y_, graph)
             score = self.multiclass_score(X, graph)
-            print('score.shape: ',score.get_shape())
             pred = self.pred_multiclass(tf.reshape(score,shape=(-1,self.nn_config['words_num'],self.nn_config['target_NETypes_num'])), tag_seq_mask, graph)
             loss = self.loss_multiclass(score, tf.reshape(Y_one_hot, shape=(-1, self.nn_config['target_NETypes_num'])), soft_log_mask, graph)
             test_loss = self.test_loss_multiclass(score, tf.reshape(Y_one_hot, shape=(-1, self.nn_config['target_NETypes_num'])), soft_log_mask, graph)
