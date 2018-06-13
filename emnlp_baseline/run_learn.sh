9a0fb4eeb5d71dd554ddd543518785b5c432221c
Yibing Liu <zapplea@gmail.com>
2018-06-13 18:58:41 +1000

    add

--- emnlp_baseline/run_learn.sh
+++ emnlp_baseline/run_learn.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --get-user-env
 #SBATCH --job-name="emnlp_baseline"
-#SBATCH --time=05:59:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=200GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
-#SBATCH --qos=express
 
 echo "loading"
 module load python/3.6.1
