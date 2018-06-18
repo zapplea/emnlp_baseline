8a050fc4aae021a5964f99a4abf494074b1bea13
Yibing Liu <zapplea@gmail.com>
2018-06-18 20:22:07 +1000

    add

--- crf_target/run_learn.sh
+++ crf_target/run_learn.sh
@@ -12,7 +12,7 @@ echo "loading"
 module load python/3.6.1
 module load cudnn/v6
 module load cuda/8.0.61
-module load tensorflow/1.8.0-py36-gpu
+module load tensorflow/1.6.0-py36-gpu
 echo "loaded"
 
 python learn.py --num $1 --dn $2
