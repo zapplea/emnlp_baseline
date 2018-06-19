a876181621f794c0d4e1705d6a68611a960fca5d
Yibing Liu <zapplea@gmail.com>
2018-06-19 17:09:46 +1000

    add

---
+++ crf_source/run_learn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --get-user-env
+#SBATCH --job-name="emnlp_baseline"
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=200GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+
+echo "loading"
+module load python/3.6.1
+module load cudnn/v6
+module load cuda/8.0.61
+module load tensorflow/1.5.0-py36-gpu
+echo "loaded"
+
+python learn.py --num $1
