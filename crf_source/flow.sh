1e6f0dc1e38bad56b1ccacbf1c2e19f9d170edf2
Yibing Liu <zapplea@gmail.com>
2018-06-21 21:33:44 +1000

    add

--- crf_source/flow.sh
+++ crf_source/flow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+rm /datastore/liu121/nosqldb2/crf_source/report/*
+rm /datastore/liu121/nosqldb2/crf_source/model/*
 sbatch run_learn.sh 0
 sbatch run_learn.sh 1
 sbatch run_learn.sh 2
--- crf_target/flow.sh
+++ crf_target/flow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+rm /datastore/liu121/nosqldb2/crf_target/report/*
+rm /datastore/liu121/nosqldb2/crf_target/model/*
 sbatch run_learn.sh 0
 sbatch run_learn.sh 1
 sbatch run_learn.sh 2
