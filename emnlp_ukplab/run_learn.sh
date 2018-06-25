72b8a79c47b3838ad17aa95d14e9b07e11ed8417
Yibing Liu <zapplea@gmail.com>
2018-06-26 00:47:03 +1000

    add

--- emnlp_ukplab/run_learn.sh
+++ emnlp_ukplab/run_learn.sh
@@ -12,9 +12,8 @@ echo "loading"
 module load python/3.6.1
 module load cudnn/v6
 module load cuda/8.0.61
-module load tensorflow/1.5.0-py36-gpu
 module load keras/2.1.3-py36
 module load protobuf/3.5.1
 
 echo "loaded"
-
+python Train_NER_Eng.py
