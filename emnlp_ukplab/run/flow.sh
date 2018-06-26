406834b227270f2e5cdb7ebcf809372bb40dd020
Yibing Liu <zapplea@gmail.com>
2018-06-27 01:31:16 +1000

    add

--- emnlp_ukplab/run/flow.sh
+++ emnlp_ukplab/run/flow.sh
@@ -3,6 +3,6 @@
 rm /datastore/liu121/nosqldb2/emnlp_ukplab/models/*
 rm /datastore/liu121/nosqldb2/emnlp_ukplab/data/pkl/*
 
-sbatch run_learn.sh bbn
+sbatch run_learn.sh bbn_kn
 sbatch run_learn.sh cadec
 sbatch run_learn.sh nvd
