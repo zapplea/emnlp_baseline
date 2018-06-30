5e1d480f3de1d7bb4a4bf52fffbb0f2cf78d47f1
Yibing Liu <zapplea@gmail.com>
2018-06-30 17:09:35 +1000

    add

---
+++ acl_hscrf/run_learn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --get-user-env
+#SBATCH --job-name="emnlp_baseline"
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=200GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+
+echo "loading"
+module load python/2.7.13
+module load cudnn/v6
+module load cuda/8.0.61
+module load pytorch/0.2.0p3-py27
+echo "loaded"
+
+python ../train.py
