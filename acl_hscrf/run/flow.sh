f73cb6d9128bb1113c9fa310441244ccaee3e377
Yibing Liu <zapplea@gmail.com>
2018-07-16 22:44:43 +1000

    add

--- acl_hscrf/run/flow.sh
+++ acl_hscrf/run/flow.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [ $1 = "bbn_kn" ];
+if [ $1 = "bbn_kn" ]
 then
     sbatch run_learn.sh bbn_kn "1.0"
-    sbatch run_learn.sh bbn_kn "2.0"
-    sbatch run_learn.sh bbn_kn "4.0"
-    sbatch run_learn.sh bbn_kn "8.0"
-    sbatch run_learn.sh bbn_kn "16.0"
-elif [ $1 = "nvd" ];
+#    sbatch run_learn.sh bbn_kn "2.0"
+#    sbatch run_learn.sh bbn_kn "4.0"
+#    sbatch run_learn.sh bbn_kn "8.0"
+#    sbatch run_learn.sh bbn_kn "16.0"
+elif [ $1 = "nvd" ]
 then
     sbatch run_learn.sh nvd "1.0"
     sbatch run_learn.sh nvd "2.0"
     sbatch run_learn.sh nvd "4.0"
     sbatch run_learn.sh nvd "8.0"
     sbatch run_learn.sh nvd "16.0"
-elif [ $1 = "cadec" ];
+elif [ $1 = "cadec" ]
 then
     sbatch run_learn.sh cadec "1.0"
     sbatch run_learn.sh cadec "2.0"
