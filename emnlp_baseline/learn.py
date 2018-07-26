f55b6ec693700f419ff663f941dafeead36ad4a5
Yibing Liu <zapplea@gmail.com>
2018-07-26 22:58:01 +1000

    add

--- emnlp_baseline/learn.py
+++ emnlp_baseline/learn.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
                      'stage1':args.stage1,
                      'dropout':0.5,
                      'bilstm_num_layers':1,
-                     'early_stop':5,
+                     'early_stop':50,
                      }
 
         lr = [0.003,]
