019903507b4a70a801c4443d8410c406635c413b
Yibing Liu <zapplea@gmail.com>
2018-07-30 21:32:33 +1000

    add

--- acl_hscrf/run/flow.sh
+++ acl_hscrf/run/flow.sh
@@ -7,6 +7,13 @@ then
     sbatch run_learn.sh bbn_kn "4.0"
     sbatch run_learn.sh bbn_kn "8.0"
     sbatch run_learn.sh bbn_kn "16.0"
+elif [ $1 = "cadec" ]
+then
+    sbatch run_learn.sh cadec "1.2"
+    sbatch run_learn.sh cadec "2.0"
+    sbatch run_learn.sh cadec "4.0"
+    sbatch run_learn.sh cadec "8.0"
+    sbatch run_learn.sh cadec "16.0"
 elif [ $1 = "nvd" ]
 then
     sbatch run_learn.sh nvd "1.0"
@@ -14,11 +21,4 @@ then
     sbatch run_learn.sh nvd "4.0"
     sbatch run_learn.sh nvd "8.0"
     sbatch run_learn.sh nvd "16.0"
-elif [ $1 = "cadec" ]
-then
-    sbatch run_learn.sh cadec "1.1"
-    sbatch run_learn.sh cadec "2.0"
-    sbatch run_learn.sh cadec "4.0"
-    sbatch run_learn.sh cadec "8.0"
-    sbatch run_learn.sh cadec "16.0"
 fi
