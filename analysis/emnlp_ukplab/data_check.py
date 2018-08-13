8b18c8eb32692f83659400454d74729cb96293a2
Yibing Liu <zapplea@gmail.com>
2018-08-13 23:45:38 +1000

    add

--- analysis/emnlp_ukplab/data_check.py
+++ analysis/emnlp_ukplab/data_check.py
@@ -37,3 +37,4 @@ if __name__=='__main__':
     data_config = {'train_conll_filePath':'/datastore/liu121/nosqldb2/emnlp_ukplab/data/bbn_kn/bbn_kn__1.0/train.txt'}
     ch=Check(data_config)
     doc,labels=ch.load()
+    ch.stat(doc,labels)
