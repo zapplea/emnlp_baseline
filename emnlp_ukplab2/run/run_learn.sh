5aff07f5f50893c6b49decfdec2659963dbf2877
Yibing Liu <zapplea@gmail.com>
2018-09-03 23:15:14 +1000

    add

--- emnlp_ukplab2/run/run_learn.sh
+++ emnlp_ukplab2/run/run_learn.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 #SBATCH --get-user-env
 #SBATCH --job-name="emnlp_baseline"
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=200GB
 #SBATCH --ntasks-per-node=1
@@ -14,6 +15,6 @@ module load cudnn/v6
 module load cuda/8.0.61
 module load keras/2.1.3-py36
 module load protobuf/3.5.1
-echo "loaded"
+echo "over"
 
 python ../Train_NER_Eng.py --mod $1 --k_shot $2
