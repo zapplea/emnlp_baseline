cb3a50bc39f256b1b9befcee5c6cf48fc7f74b97
Yibing Liu <zapplea@gmail.com>
2018-09-03 23:05:41 +1000

    add

---
+++ emnlp_ukplab2/run/flow.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ $1 = "bbn_kn" ]
+then
+    rm /datastore/liu121/nosqldb2/emnlp_ukplab/data/pkl/bbn_kn*
+    sbatch run_learn.sh bbn_kn 1.0
+    sbatch run_learn.sh bbn_kn 2.0
+    sbatch run_learn.sh bbn_kn 4.0
+    sbatch run_learn.sh bbn_kn 8.0
+    sbatch run_learn.sh bbn_kn 16.0
+elif [ $1 = "cadec" ]
+then
+    rm /datastore/liu121/nosqldb2/emnlp_ukplab/data/pkl/cadec*
+    sbatch run_learn.sh cadec 1.0
+    sbatch run_learn.sh cadec 2.0
+    sbatch run_learn.sh cadec 4.0
+    sbatch run_learn.sh cadec 8.0
+    sbatch run_learn.sh cadec 16.0
+elif [ $1 = "nvd" ]
+then
+    rm /datastore/liu121/nosqldb2/emnlp_ukplab/data/pkl/nvd*
+    sbatch run_learn.sh nvd 1.0
+    sbatch run_learn.sh nvd 2.0
+    sbatch run_learn.sh nvd 4.0
+    sbatch run_learn.sh nvd 8.0
+    sbatch run_learn.sh nvd 16.0
+fi
