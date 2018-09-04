c3686be6d0665fd3bfd786b90251679e070c7f8a
Yibing Liu <zapplea@gmail.com>
2018-09-05 04:51:15 +1000

    add

--- emnlp_ukplab2/Train_NER_Eng.py
+++ emnlp_ukplab2/Train_NER_Eng.py
@@ -109,7 +109,7 @@ embeddings, mappings, data_test = loadDatasetPickle(pickleFile_test)
 
 # Some network hyperparameters
 params = {'classifier': ['CRF'], 'LSTM-Size': [100, 100], 'dropout': (0.25, 0.25), 'charEmbeddings': 'CNN', 'maxCharLength': 50,
-          'featureNames': ['tokens'],'earlyStopping':100,'wordEmbeddings_trainable':True}
+          'featureNames': ['tokens','casing'],'earlyStopping':100,'wordEmbeddings_trainable':True}
 
 
 print('#######################'+args.mod+' #######################')
